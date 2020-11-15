b91ca70b327550e651caae4046dd12cfeafa80f0
se1exin <selexin@selexin.com>
2020-11-15 18:39:54 +1100

    Save servo positions to file

--- app.py
+++ app.py
@@ -1,15 +1,57 @@
+import configparser
 import time
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 from servo import move_servo
 
 app = Flask(__name__)
 CORS(app)
 
+
+_FILENAME = "servocam.ini"
+_POSITION_KEY = "position"
 servo_x_pos = 90
 servo_y_pos = 90
 servo_locked = False
+
+
+config = configparser.ConfigParser()
+
+
+def save_positions(x, y):
+    try:
+        config[_POSITION_KEY]["x"] = x
+        config[_POSITION_KEY]["y"] = y
+        with open(_FILENAME, "w") as configfile:
+            config.write(configfile)
+    except:
+        pass
+
+
+def get_saved_positions():
+    _config = config.read(_FILENAME)
+
+    x = servo_x_pos
+    y = servo_y_pos
+
+    if _POSITION_KEY in _config:
+        x = _config[_POSITION_KEY].getint("x", x)
+        y = _config[_POSITION_KEY].getint("y", y)
+
+    return x, y
+
+
+@app.errorhandler(Exception)
+def internal_error(error):
+    return jsonify({"success": False, "error": str(error)})
+
+
+@app.route("/servo", methods=["GET"])
+def get_position():
+    x, y = get_saved_positions()
+    return jsonify({"success": True, "x": x, "y": y})
 
 
 @app.route("/servo", methods=["POST"])
@@ -41,12 +83,15 @@ def update_position():
 
     servo_locked = True
     move_servo(axis, new_pos)
-    servo_locked = False
 
     if axis == "x":
         servo_x_pos = new_pos
     if axis == "y":
         servo_y_pos = new_pos
+
+    save_positions(servo_x_pos, servo_y_pos)
+
+    servo_locked = False
 
     return jsonify({"success": True})
 
@@ -65,6 +110,7 @@ def reset_potition():
     servo_locked = True
     move_servo("x", servo_x_pos)
     move_servo("y", servo_y_pos)
+    save_positions(servo_x_pos, servo_y_pos)
     servo_locked = False
 
     return jsonify({"success": True})
